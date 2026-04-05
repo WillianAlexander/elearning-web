e320af283b8faad6c1829032e9ef808f1a70ad86
kxdiii <alexfj-789@hotmail.com>
2026-04-05 14:18:03 -0500

    fix: update API URL to elearning-api.spayzly.pro

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   staging: false,
   devMode: false,
-  apiUrl: 'https://api-lms.spayzly.pro/api/v1',
+  apiUrl: 'https://elearning-api.spayzly.pro/api/v1',
   azure: {
     clientId: 'f0c9e043-990e-4213-b740-ae078665f8c3',
     authority: 'https://login.microsoftonline.com/1c82b496-37c3-4c4b-97ca-77aa4a47ab2f',
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   staging: true,
   devMode: true,
-  apiUrl: 'https://api-lms.spayzly.pro/api/v1',
+  apiUrl: 'https://elearning-api.spayzly.pro/api/v1',
   azure: {
     clientId: 'f0c9e043-990e-4213-b740-ae078665f8c3',
     authority: 'https://login.microsoftonline.com/1c82b496-37c3-4c4b-97ca-77aa4a47ab2f',
