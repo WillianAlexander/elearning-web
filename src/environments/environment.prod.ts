6bcfe864dbda875c13eeda36e94f41f1675040e4
kxdiii <alexfj-789@hotmail.com>
2026-04-05 14:19:46 -0500

    fix: update redirect URI to elearningv2.spayzly.pro

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   azure: {
     clientId: 'f0c9e043-990e-4213-b740-ae078665f8c3',
     authority: 'https://login.microsoftonline.com/1c82b496-37c3-4c4b-97ca-77aa4a47ab2f',
-    redirectUri: 'https://elearning.spayzly.pro/auth/callback',
+    redirectUri: 'https://elearningv2.spayzly.pro/auth/callback',
     scopes: ['openid', 'profile', 'email', 'User.Read'],
   },
 };
